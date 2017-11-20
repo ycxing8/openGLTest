d7a454421c5f90b7e85324acb90096e5ef21ecb4
xingyongchen <ycxing08@outlook.com>
2017-11-20 23:58:15 +0800

    画三角形

--- GLTest2/GLTest2.cpp
+++ GLTest2/GLTest2.cpp
@@ -2,10 +2,30 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "glut.h"
 
-
-int main()
+void display()
 {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 0.5);
+	glVertex2f(-0.5, -0.5);
+	glVertex2f(0.5, -0.5);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char **argv)
+{
+	FreeConsole();//¹Ø±Õ¿ØÖÆÌ¨
+	glutInit(&argc, argv);
+	glutCreateWindow("triangle");
+	glutDisplayFunc(display);
+	glutMainLoop();
     return 0;
 }
+
 
